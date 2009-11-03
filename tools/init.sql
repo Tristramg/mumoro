4043ff5c50f5251d6d0f03310fcc1ef8abe249db
Tristram Gräbener <tristramg@gmail.com>
2009-11-03 13:47:43 +0100

    Removed osm parser as a seperated tool osm4routing has been created

--- tools/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE links(id int primary key, source int, target int, geom text, bike bool, bike_r bool, car bool, car_r bool, foot bool, length double);
-CREATE TABLE nodes(id int primary key, lon double, lat double);
-CREATE INDEX geom_idx on nodes(lat,lon);
