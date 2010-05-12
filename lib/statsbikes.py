4d191b03f100a0aad6903d551db696880054a755
ody <ody@ody-notebook-gnome.(none)>
2010-05-12 12:06:59 +0200

    updated stats for the new table

--- lib/statsbikes.py
+++ lib/statsbikes.py
@@ -15,8 +15,16 @@ class StatsBikes:
             self.conn = pg.connect( tmp )
         except:
             print "I am unable to connect to the database"
+        cur = self.conn.cursor()
+        query = "TRUNCATE TABLE bike_stations"
+	try:        
+	    cur.execute( query )
+	except Exception as ex:
+            print "I am unable to empty data from the database"
+            print ex
         for i in s.stations:
             self.addStationData(i)
+        self.conn.commit()
         self.conn.close()
     def addStationData(self,s):
 	cur = self.conn.cursor()
@@ -26,10 +34,14 @@ class StatsBikes:
 	try:        
 	    cur.execute( query , [ day, s['num'], s['availableSlots'], s['availableBikes'], chrone ] )
 	except Exception as ex:
-            print "I am unable to insert data into the database"
+            print "I am unable to insert bike stations stats data into the database"
             print ex
-        self.conn.commit()
-        
+        query = "INSERT INTO bike_stations (\"id_station\", \"av_bikes\", \"av_slots\", \"name\", \"district_name\", \"lon\", \"lat\") VALUES (%s, %s, %s, %s, %s, %s, %s)"
+	try:        
+	    cur.execute( query , [ s['num'], s['availableBikes'], s['availableSlots'], s['name'], s['districtName'], s['lon'], s['lat'] ] )
+	except Exception as ex:
+            print "I am unable to insert bike stations stats data into the database"
+            print ex
 if __name__ == "__main__":
     x=StatsBikes()
         
