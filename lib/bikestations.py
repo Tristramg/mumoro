ca84cb79e14690de3468fc197a3362cd45161500
ody <ody@ody-notebook-gnome.(none)>
2010-05-14 13:43:47 +0200

    Small change

--- lib/bikestations.py
+++ lib/bikestations.py
@@ -3,6 +3,7 @@ import urllib
 import config
 import psycopg2 as pg
 from xml.dom.minidom import parse
+import time
 
 def get_text(node):
     return node.childNodes[0].nodeValue
@@ -106,7 +107,7 @@ class VeloStar:
                 res += ( textOrange + '%i Available slots</span><br>' % s['availableSlots'])
             else:
                 res += ( title + '%i Available slots</span><br>' % s['availableSlots'] )
-            res += ('<br>' + tinyText + 'Latest update on :' + str(s['chrone']) + '</span>')
+            res += ('<br>' + tinyText + 'Latest update on : ' + time.strftime("%H:%M", s['chrone']) + '</span>')
             res += ('<br>' + tinyText + 'Click again to close</span></div>\t')
             if (s['availableSlots'] == 0 or s['availableBikes'] == 0 or ['state'] == 0):
                 res += ('img/bike.station.red.png\t18,25\t-8,-25\n')
