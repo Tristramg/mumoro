3671fa4b92a40a139bc9e9474b48c8318cd2a306
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2010-06-10 14:10:36 +0200

    Added correct unittest (all tests fail)

---
+++ test/testunit.py
@@ -0,0 +1,33 @@
+import unittest
+
+class TestSequenceFunctions(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_import_street_data(self):
+        self.assertTrue(False)
+        
+    def test_import_municipal_data(self):
+        self.assertTrue(False)
+
+    def test_import_bike_service(self):
+        self.assertTrue(False)
+        
+    def test_load_layer(self):
+        self.assertTrue(False)
+
+    def test_cost(self):
+        self.assertTrue(False)
+        
+    def test_connect_layers_same_nodes(self):
+        self.assertTrue(False)
+    
+    def test_connect_layers_from_node_list(self):
+        self.assertTrue(False)
+        
+    def test_create_multimodal_graph(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
