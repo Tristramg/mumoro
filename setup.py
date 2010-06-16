15a0039b666cb3710a2783695bb73e73a6f660c2
Tristram Gräbener <tristramg@gmail.com>
2010-06-16 17:09:35 +0200

    Improved packaging

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
+from distribute_setup import use_setuptools
+use_setuptools()
+
 from setuptools import setup, find_packages, Extension
 setup(name='Mumoro',
-      version='1.0',
-      author= 'Tristram',
+      version='0.0.1b',
+      author= 'Tristram Graebener',
       author_email = 'tristramg@gmail.com',
       url = 'http://github.com/Tristramg/mumoro/',
+      description = 'Multimodal and multiobjective routing',
+      licence = 'GPLv3',
       packages = ['lib', 'lib.core', 'web'],
 
-      install_requires = ['cherrypy', 'genshi', 'simplejson', 'transitfeed', 'psycopg2', 'setuptools-git'],
+      install_requires = ['cherrypy', 'genshi', 'simplejson', 'transitfeed', 'setuptools-git', 'osm4routing'],
       ext_modules = [
           Extension("lib.core._mumoro",
-              sources=["lib/core/martins.cpp", "lib/core/graph_wrapper.cpp", "lib/core/mumoro_wrap.cpp"],
+              sources=["lib/core/martins.cpp", "lib/core/graph_wrapper.cpp", "lib/core/mumoro.i"],
               swig_opts=['-c++'],
               include_dirs=['lib/core/'])
           ]
