92136d1096bd4b09aca340ad5f9d6c8574f224f7
Tristram Gräbener <tristramg@gmail.com>
2010-06-29 18:29:29 +0200

    The gtfs conversion script now adds services (as bitset givent two date intervals)

--- lib/data_manipulation/gtfs_to_sqlite.py
+++ lib/data_manipulation/gtfs_to_sqlite.py
@@ -1,7 +1,8 @@
 import sqlite3
 import transitfeed
+import datetime
 
-def convert(gtfs, sqlite, network_name, start, end):
+def convert(gtfs, sqlite, network_name, start_date, end_date):
     s = transitfeed.Schedule()
     s.Load(gtfs)
     sql = sqlite3.connect(sqlite)
@@ -37,30 +38,39 @@ def convert(gtfs, sqlite, network_name, start, end):
     else:
         count = 0
     query_nodes = "INSERT INTO nodes (id, original_id, lon, lat, network, route) VALUES(?, ?, ?, ?, ?, ?)"
-    query_edges = "INSERT INTO edges (source, target, start_secs, arrival_secs) VALUES(?, ?, ?, ?)" 
+    query_edges = "INSERT INTO edges (source, target, start_secs, arrival_secs, services) VALUES(?, ?, ?, ?, ?)" 
     for trip in s.GetTripList():
-        if s.GetServicePeriod(trip.service_id).IsActiveOn("20100430"):
-            if not map.has_key(trip.route_id):
-                map[trip.route_id] = {}
-            prev_time = None
-            prev_stop = None
-            current_stop = None
-            for stop in trip.GetStopTimes():
-                if not map[trip.route_id].has_key(stop.stop_id):
-                    map[trip.route_id][stop.stop_id] = count
-                    sql.execute(query_nodes, (count, stop.stop_id, stop.stop.stop_lon, stop.stop.stop_lat, network_name, trip.route_id))
-                    count +=1
-                current_stop = map[trip.route_id][stop.stop_id]
-                if prev_stop != None:
-                    sql.execute(query_edges, (prev_stop, current_stop, prev_time, stop.arrival_secs))
-                prev_stop = current_stop
-                prev_time = stop.departure_secs
-
+        service_period = s.GetServicePeriod(trip.service_id)
+        services = ""
+        delta = datetime.timedelta(days=1)
+        date = start_date
+        while date <= end_date:
+            if service_period.IsActiveOn(date.strftime("%Y%m%d"), date_object=date):
+                services = "1" + services
+            else:
+                services = "0" + services
+            date += delta
+            
+        if not map.has_key(trip.route_id):
+            map[trip.route_id] = {}
+        prev_time = None
+        prev_stop = None
+        current_stop = None
+        for stop in trip.GetStopTimes():
+            if not map[trip.route_id].has_key(stop.stop_id):
+                map[trip.route_id][stop.stop_id] = count
+                sql.execute(query_nodes, (count, stop.stop_id, stop.stop.stop_lon, stop.stop.stop_lat, network_name, trip.route_id))
+                count +=1
+            current_stop = map[trip.route_id][stop.stop_id]
+            if prev_stop != None:
+                sql.execute(query_edges, (prev_stop, current_stop, prev_time, stop.arrival_secs, services))
+            prev_stop = current_stop
+            prev_time = stop.departure_secs
 
     sql.commit()
 
 
 if __name__ == '__main__':
-    print "hello world"
-    convert('la.zip', 'la', 'metro')
-    #convert('bart.zip', 'pt2', 'bart')
+    #print "hello world"
+    #convert('la.zip', 'la', 'metro')
+    convert('bart.zip', 'pt2', 'bart', datetime.date(2010, 06, 01),datetime.date(2010,9, 01))
